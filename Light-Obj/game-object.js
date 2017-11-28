aba3cbf289acdb3f5adfddff4b1326c8797f6c4c
Muvaffak Onus <onus.muvaffak@gmail.com>
2017-11-28 15:12:17 +0300

    second demo. light is still directional

--- Light-Obj/game-object.js
+++ Light-Obj/game-object.js
@@ -1,9 +1,9 @@
 class GameObject
 {
-    constructor(mesh){
+    constructor(mesh, position){
         var program = initShaders(gl, 'vertex-shader-object', 'fragment-shader-object');
         program.mvp = gl.getUniformLocation(program, "mvp");
-        program.lightDir = gl.getUniformLocation(program, "lightDir");
+        program.lightPos = gl.getUniformLocation(program, "lightPos");
         program.cameraPos = gl.getUniformLocation(program, "cameraPos");
         program.screenSize = gl.getUniformLocation(program, "screenSize");
 
@@ -15,27 +15,27 @@ class GameObject
 
         this.program = program;
         this.mesh = mesh;
-        this.model = mat4();
+        this.model = position || mat4();
     }
 
-    draw(cameraPos, lightDir, viewProjection, model){
+    draw(cameraPos, lightPos, viewProjection, model){
         model = model || mat4();
         model = mult(this.model, model);
         viewProjection = viewProjection || mat4();
 
         gl.useProgram(this.program);
 
-        
+
         gl.uniformMatrix4fv(this.program.mvp, false, flatten(mult(viewProjection, model)));
-        gl.uniform3f(this.program.lightDir, lightDir[0], lightDir[1], lightDir[2]);
+        gl.uniform3f(this.program.lightPos, lightPos[0], lightPos[1], lightPos[2]);
         gl.uniform3f(this.program.cameraPos, cameraPos[0], cameraPos[1], cameraPos[2]);
-        
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.mesh.vertexBuffer);
         gl.vertexAttribPointer(this.program.vpos_attr, this.mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.mesh.normalBuffer);
         gl.vertexAttribPointer(this.program.vnor_attr, this.mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        
+
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.mesh.indexBuffer);
         gl.drawElements(gl.TRIANGLES, this.mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
     }
--- Light-Obj/light-obj.js
+++ Light-Obj/light-obj.js
@@ -1,19 +1,30 @@
 var sphere;
+var plane;
+var block;
 var models = {};
 
-var lightDir = vec3(0, -1, -1);
+var lightPos = vec3(0, -3, 0);
 
 var at = vec3(0, 0, 0);
-var eye = vec3(0, 0, 5);
+var eye = vec3(0, 0, 10);
 var toCam = subtract(eye, at);
 var proj;
 var view;
 
+var displacement = mult(mat4(), translate(0, -1, 5));
+
 
 function modelLoad(meshes) {
     models.meshes = meshes;
+
+    OBJ.initMeshBuffers(gl, models.meshes.plane);
     OBJ.initMeshBuffers(gl, models.meshes.sphere);
+    OBJ.initMeshBuffers(gl, models.meshes.block);
+
+    plane = new GameObject(models.meshes.plane, mult(mat4(), translate(0,-2.5,-2)));
     sphere = new GameObject(models.meshes.sphere);
+    block = new GameObject(models.meshes.block, mult(mat4(), translate(5, -2.5, 0)));
+
     gameLoop();
 }
 
@@ -31,7 +42,9 @@ window.onload = function () {
     gl.clearColor(0, 1.0, 0, 1);
 
     OBJ.downloadMeshes({
-        'sphere': 'sphere.obj'
+      'plane': 'plane.obj',
+      'sphere': 'sphere.obj',
+      'block': 'block.obj'
     }, modelLoad);
 
 
@@ -43,14 +56,32 @@ const gameLoop = function () {
     gl.enable(gl.DEPTH_TEST);
 
     if (!gameEnded) {
+      view = lookAt(add(at, toCam), at, [0, 1, 0]);
+      proj = perspective(45, screenSize[0] / screenSize[1], 0.1, 10);
 
-        view = lookAt(add(at, toCam), at, [0, 1, 0]);
-        proj = perspective(45, screenSize[0] / screenSize[1], 0.1, 10);
+      plane.draw(eye, lightPos, mult(proj,view));
+      sphere.draw(eye, lightPos, mult(proj,view), displacement);
+      block.draw(eye, lightPos, mult(proj,view));
 
-        sphere.draw(eye, lightDir, mult(proj,view));
-        window.requestAnimationFrame(gameLoop);
+      window.requestAnimationFrame(gameLoop);
     }
     else {
         window.cancelAnimationFrame(gameLoop);
     }
+}
+
+document.onkeydown = function(key){
+  console.log(key.keyCode);
+  if (key.keyCode == 39) {
+    displacement = mult(displacement, translate(0.1, 0, 0));
+  }
+  else if (key.keyCode == 37) {
+    displacement = mult(displacement, translate(-0.1, 0, 0));
+  }
+  else if (key.keyCode == 38) {
+    displacement = mult(displacement, translate(0, 0, -0.1));
+  }
+  else if (key.keyCode == 40) {
+    displacement = mult(displacement, translate(0, 0, 0.1));
+  }
 }
